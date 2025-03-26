f098ffcf7b1012081d71718b8a8b526da59a6d15
frankyrossy <rosssler.frank@gmail.com>
2025-03-26 19:07:56 +0000

    header

---
+++ krankentransport/public/components/footer.js
@@ -0,0 +1,13 @@
+function createFooter() {
+  return `
+    <footer>
+      <div class="container">
+        <div class="contact-info">
+          <p>Telefon: +49 123 456789</p>
+          <p>Email: info@krankentransport.de</p>
+        </div>
+        <p>&copy; ${new Date().getFullYear()} Krankentransport App. Alle Rechte vorbehalten.</p>
+      </div>
+    </footer>
+  `;
+}
---
+++ krankentransport/public/components/header.js
@@ -0,0 +1,20 @@
+function createHeader() {
+  return `
+    <header>
+      <div class="container">
+        <div class="logo">
+          <img src="images/ambulance-icon.svg" alt="Krankentransport Logo">
+          <span>Krankentransport</span>
+        </div>
+        <nav>
+          <ul>
+            <li><a href="index.html">Startseite</a></li>
+            <li><a href="leistungen.html">Leistungen</a></li>
+            <li><a href="ueber-uns.html">Über uns</a></li>
+            <li><a href="kontakt.html">Kontakt</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  `;
+}
