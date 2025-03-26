da32e7b7186a7e9e11ace7076b9cc9122f70b9db
frankyrossy <rosssler.frank@gmail.com>
2025-03-26 19:15:16 +0000

    h

--- krankentransport/public/components/header.js
+++ krankentransport/public/components/header.js
@@ -1,20 +1,31 @@
+// header.js
 function createHeader() {
   return `
-    <header>
-      <div class="container">
-        <div class="logo">
-          <img src="images/ambulance-icon.svg" alt="Krankentransport Logo">
-          <span>Krankentransport</span>
+    <header class="main-header">
+      <div class="header-container">
+        <div class="logo-container">
+          <img src="images/ambulance-icon.svg" 
+               alt="Krankentransport Logo" 
+               class="logo-img"
+               width="36" 
+               height="36">
+          <h1 class="logo-text">Terminbuchung</h1>
         </div>
-        <nav>
-          <ul>
-            <li><a href="index.html">Startseite</a></li>
-            <li><a href="leistungen.html">Leistungen</a></li>
-            <li><a href="ueber-uns.html">Über uns</a></li>
-            <li><a href="kontakt.html">Kontakt</a></li>
+        
+        <nav class="main-nav">
+          <ul class="nav-list">
+            <li class="nav-item"><a href="index.html" class="nav-link active">Startseite</a></li>
+            <li class="nav-item"><a href="leistungen.html" class="nav-link">Leistungen</a></li>
+            <li class="nav-item"><a href="ueber-uns.html" class="nav-link">Über uns</a></li>
+            <li class="nav-item"><a href="kontakt.html" class="nav-link cta-button">Kontakt</a></li>
           </ul>
         </nav>
       </div>
     </header>
   `;
 }
+
+// Header automatisch einbinden
+document.addEventListener("DOMContentLoaded", () => {
+  document.body.insertAdjacentHTML('afterbegin', createHeader());
+});
